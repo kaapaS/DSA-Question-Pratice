2a56b8044a02ecbe3b4c572027ed2d348b244a44
singhKalpana <127042824+kaapaS@users.noreply.github.com>
2024-02-19 00:05:06 +0530

    Time: 3 ms (47.94%) | Memory: 11.3 MB (6.20%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,24 +1,26 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-class Solution{
-    public: bool uniqueOccurrences(std::vector<int>& arr) {
-    std::unordered_map<int, int> count;
-    std::unordered_set<int> uniqueCounts;
 
-    // Count occurrences of each value
-    for (int num : arr) {
-        count[num]++;
-    }
+class Solution {
+public:
+    bool uniqueOccurrences(std::vector<int>& arr) {
+        std::unordered_map<int, int> count;
+        std::unordered_set<int> uniqueCounts;
 
-    // Check if the counts are unique
-    for (const auto& pair : count) {
-        if (!uniqueCounts.insert(pair.second).second) {
-            return false; // Duplicate occurrence count found
+        // Count occurrences of each value
+        for (int num : arr) {
+            count[num]++;
         }
+
+        // Check if the counts are unique
+        for (const auto& pair : count) {
+            if (!uniqueCounts.insert(pair.second).second) {
+                return false; // Duplicate occurrence count found
+            }
+        }
+
+        return true;
     }
-
-    return true;
-}
-
 };
+
